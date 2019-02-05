9839ae95577e843732b99cdb3a6546714a132e8d
Stephen Widom <stephen.widom@strategic-i.com>
2019-02-04 22:54:21 -0500

    Add resume link, nav styles

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,16 +2,17 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 
 const Header = (props) => (
-        <header>
-            <nav className="container">
-                <ul>
-                    <li><NavLink to="/">Home</NavLink></li>
-                    <li><NavLink to="/portfolio">Portfolio</NavLink></li>
-                    <li><NavLink to="/technologies">Technologies</NavLink></li>
-                    <li><NavLink to="/skills">Skills</NavLink></li>
-                </ul>
-            </nav>
-        </header>
+    <header>
+        <nav className="container">
+            <ul>
+                <li><NavLink to="/" exact={true}>Home</NavLink></li>
+                <li><NavLink to="/portfolio">Portfolio</NavLink></li>
+                <li><NavLink to="/technologies">Technologies</NavLink></li>
+                <li><NavLink to="/skills">Skills</NavLink></li>
+                <li><NavLink to="/resume">Resume</NavLink></li>
+            </ul>
+        </nav>
+    </header>
 );
 
 export default Header;
---
+++ src/components/Resume.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class Resume extends React.Component {
+
+    constructor(props) {
+        super (props);
+    }
+
+    render() {
+        return (
+            <div id="resume">Resume placeholder</div>
+        );
+    }
+}
+
+export default Resume;
