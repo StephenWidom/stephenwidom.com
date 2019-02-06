a03f241f53111536f1dceefe2791f0fabcd7e2b1
Stephen Widom <stevewidom@gmail.com>
2019-02-05 23:02:38 -0500

    Got header transition down

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,42 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import Transition from 'react-transition-group/Transition';
 
-const Header = (props) => (
-    <header>
-        <nav className="container">
-            <ul>
-                <li><NavLink to="/" exact={true}>Home</NavLink></li>
-                <li><NavLink to="/portfolio">Portfolio</NavLink></li>
-                <li><NavLink to="/technologies">Technologies</NavLink></li>
-                <li><NavLink to="/skills">Skills</NavLink></li>
-                <li><NavLink to="/resume">Resume</NavLink></li>
-            </ul>
-        </nav>
-    </header>
-);
+const Header = (props) => {
+
+    const defaultStyle = {
+        transform: "translateY(-50px)",
+        transition: "transform 0.5s ease",
+        background: "#ffffff"
+    }
+
+    const transitionStyles = {
+        entering: {},
+        entered: { transform: "translateY(0)"}
+    }
+
+    return (
+
+        <Transition
+            in={true}
+            appear={true}
+            timeout={1900}
+        >
+            {(state) => (
+            <header style={{...defaultStyle, ...transitionStyles[state]}}>
+                <nav className="container">
+                    <ul>
+                        <li><NavLink to="/" exact={true}>Home</NavLink></li>
+                        <li><NavLink to="/portfolio">Portfolio</NavLink></li>
+                        <li><NavLink to="/technologies">Technologies</NavLink></li>
+                        <li><NavLink to="/skills">Skills</NavLink></li>
+                        <li><NavLink to="/resume">Resume</NavLink></li>
+                    </ul>
+                </nav>
+            </header>
+            )}
+        </Transition>
+    )
+}
 
 export default Header;
