b516eecf446d6a650f86b60898cf452574c0de0b
Stephen Widom <stephen.widom@strategic-i.com>
2019-02-11 17:47:19 -0500

    Cache about section too

--- src/components/About.js
+++ src/components/About.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactHtmlParser from 'react-html-parser';
+import Loading from './Loading';
 
 class About extends React.Component {
 
@@ -7,27 +8,50 @@ class About extends React.Component {
         super(props);
 
         this.state = {
-            content: null
+            about: null,
+            isLoading: true
         }
     }
 
     componentWillMount() {
+        if (this.props.about !== null) {
+            this.setState(() => {
+                return {
+                    about: this.props.about,
+                    isLoading: false
+                }
+            });
+        } else {
+            this.getAbout();
+        }
+    }
+
+    getAbout() {
         const endpoint = '/cms/wp-json/wp/v2/pages?slug=about';
         const myRequest = new Request(endpoint);
         fetch(myRequest).then((response) => response.json()).then((response) => {
-            const content = response[0].content.rendered;
+            const about = response[0].content.rendered;
             this.setState(() => {
-                return { content }
-            }, () => console.log(this.state));
+                return { about }
+            }, () => {
+                this.setState(() => {
+                    return { isLoading: false }
+                });
+                this.props.updateCache('about', about);
+            });
         });
     }
 
     render() {
         return (
             <section id="about">
+                {this.state.isLoading && (
+                <Loading />
+                ) || (
                 <div class="container content">
-                    {this.state.content != null && ReactHtmlParser(this.state.content)}
+                    {this.state.about != null && ReactHtmlParser(this.state.about)}
                 </div>
+                )}
             </section>
         );
     }
