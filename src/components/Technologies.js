0d633fd833636cb73b495f50466a9ad431c3d7b5
Stephen Widom <stevewidom@gmail.com>
2019-02-15 00:32:56 -0500

    Grab technologies, skills on component mount

--- src/components/Technologies.js
+++ src/components/Technologies.js
@@ -1,29 +1,72 @@
 import React from 'react';
 import Technology from './Technology';
+import Loading from './Loading';
 
-const Technologies = (props) => {
+class Technologies extends React.Component {
 
-    const techContainerStyles = {
-        display: 'flex',
-        flexWrap: 'wrap',
-        justifyContent: 'center',
-    };
+    constructor(props) {
+        super(props);
+        this.state = {
+            technologies: null,
+            isLoading: true
+        }
+    }
 
-    return (
-        <section id="technologies">
-            {typeof props.technologies !== undefined && (
-                <div className="container">
-                    <h2>Technologies</h2>
-                    <div style={techContainerStyles}>
-                    {props.technologies.map((item, index) =>
-                        <Technology technology={item} index={index} colors={props.colors} scale={props.scale} />
+    componentWillMount() {
+        if (this.props.technologies !== null) {
+            this.setState(() => {
+                return {
+                    technologies: this.props.technologies,
+                    isLoading: false
+                }
+            });
+        } else {
+            this.getTechnologies();
+        }
+    }
+
+    getTechnologies = () => {
+        const endpoint = "/cms/wp-json/wp/v2/technologies?per_page=100";
+        const myRequest = new Request(endpoint);
+        fetch(myRequest).then((response) => response.json()).then((technologies) => {
+            this.setState(() => {
+                return {
+                    technologies
+                }
+            }, () => {
+                this.setState(() => {
+                    return { isLoading: false }
+                });
+                this.props.updateCache('technologies', technologies);
+            });
+        });
+    }
+
+    render() {
+        const techContainerStyles = {
+            display: 'flex',
+            flexWrap: 'wrap',
+            justifyContent: 'center',
+        };
+        return (
+            <section id="technologies">
+                {this.state.isLoading && (
+                    <Loading />
+                ) || (
+                    <div className="container">
+                        <h2>Technologies</h2>
+                    {this.state.technologies != null && (
+                        <div style={techContainerStyles}>
+                        {this.state.technologies.map((item, index) =>
+                            <Technology technology={item} index={index} colors={this.props.colors} scale={this.props.scale} />
+                        )}
+                        </div>
                     )}
                     </div>
-                </div>
-            )}
-        </section>
-    )
-
+                )}
+            </section>
+        )
+    }
 }
 
 export default Technologies;
--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -21,8 +21,8 @@ const AppRouter = (props) => {
                 <Switch>
                     <Route path="/" exact render={(props) => <Intro {...props} updateCache={updateCache} about={about} />} />
                     <Route path="/portfolio" exact render={(props) => <Portfolio {...props} updateCache={updateCache} portfolio={portfolio} />} />
-                    <Route path="/technologies" render={(props) => <Technologies {...props} colors={colors} technologies={technologies} scale={scale} />} />
-                    <Route path="/skills" render={(props) => <Skills {...props} colors={colors} skills={skills} scale={scale} />} />
+                    <Route path="/technologies" render={(props) => <Technologies {...props} colors={colors} technologies={technologies} scale={scale} updateCache={updateCache} />} />
+                    <Route path="/skills" render={(props) => <Skills {...props} colors={colors} skills={skills} scale={scale} updateCache={updateCache} />} />
                     <Route path="/fun" exact render={(props) => <Fun {...props} updateCache={updateCache} slackmojis={slackmojis} />} />
                     <Route path="/resume" exact render={(props) => <Resume {...props} updateCache={updateCache} resume={resume} />} />
                     <Route component={NotFound} />
